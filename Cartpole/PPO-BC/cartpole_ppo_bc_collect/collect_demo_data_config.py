2c301e13b27c053eada10c3fe6d91d8c22eb4b6e
shenpengfii <2134452828@qq.com>
2023-07-20 09:59:39 +0800

    add PPO as expert and for debugging

---
+++ Cartpole/PPO-BC/cartpole_ppo_bc_collect/collect_demo_data_config.py
@@ -0,0 +1,128 @@
+exp_config = {
+    'env': {
+        'manager': {
+            'episode_num': float("inf"),
+            'max_retry': 1,
+            'retry_type': 'reset',
+            'auto_reset': True,
+            'step_timeout': None,
+            'reset_timeout': None,
+            'retry_waiting_time': 0.1,
+            'cfg_type': 'BaseEnvManagerDict',
+            'type': 'base'
+        },
+        'stop_value': 195,
+        'n_evaluator_episode': 5,
+        'type': 'cartpole',
+        'import_names': ['dizoo.classic_control.cartpole.envs.cartpole_env'],
+        'collector_env_num': 8,
+        'evaluator_env_num': 5
+    },
+    'policy': {
+        'model': {
+            'obs_shape': 4,
+            'action_shape': 2,
+            'encoder_hidden_size_list': [64, 64, 128],
+            'critic_head_hidden_size': 128,
+            'actor_head_hidden_size': 128,
+            'action_space': 'discrete'
+        },
+        'learn': {
+            'learner': {
+                'train_iterations': 1000000000,
+                'dataloader': {
+                    'num_workers': 0
+                },
+                'log_policy': True,
+                'hook': {
+                    'load_ckpt_before_run': '',
+                    'log_show_after_iter': 100,
+                    'save_ckpt_after_iter': 1000,
+                    'save_ckpt_after_run': True
+                },
+                'cfg_type': 'BaseLearnerDict'
+            },
+            'update_per_collect': 6,
+            'batch_size': 64,
+            'learning_rate': 0.001,
+            'value_weight': 0.5,
+            'entropy_weight': 0.01,
+            'clip_ratio': 0.2,
+            'adv_norm': False,
+            'ignore_done': False
+        },
+        'collect': {
+            'collector': {
+                'deepcopy_obs': False,
+                'transform_obs': False,
+                'collect_print_freq': 100,
+                'cfg_type': 'SampleSerialCollectorDict',
+                'type': 'sample'
+            },
+            'unroll_len': 1,
+            'discount_factor': 0.9,
+            'gae_lambda': 0.95,
+            'n_sample': 128
+        },
+        'eval': {
+            'evaluator': {
+                'eval_freq': 40,
+                'render': {
+                    'render_freq': -1,
+                    'mode': 'train_iter'
+                },
+                'cfg_type': 'InteractionSerialEvaluatorDict',
+                'stop_value': 195,
+                'n_episode': 5
+            }
+        },
+        'other': {
+            'replay_buffer': {
+                'type': 'advanced',
+                'replay_buffer_size': 5000,
+                'max_use': float("inf"),
+                'max_staleness': float("inf"),
+                'alpha': 0.6,
+                'beta': 0.4,
+                'anneal_step': 100000,
+                'enable_track_used_data': False,
+                'deepcopy': False,
+                'thruput_controller': {
+                    'push_sample_rate_limit': {
+                        'max': float("inf"),
+                        'min': 0
+                    },
+                    'window_seconds': 30,
+                    'sample_min_limit_ratio': 1
+                },
+                'monitor': {
+                    'sampled_data_attr': {
+                        'average_range': 5,
+                        'print_freq': 200
+                    },
+                    'periodic_thruput': {
+                        'seconds': 60
+                    }
+                },
+                'cfg_type': 'AdvancedReplayBufferDict'
+            },
+            'commander': {
+                'cfg_type': 'BaseSerialCommanderDict'
+            }
+        },
+        'on_policy': False,
+        'cuda': False,
+        'multi_gpu': False,
+        'bp_update_sync': True,
+        'traj_len_inf': False,
+        'type': 'ppo_offpolicy',
+        'priority': False,
+        'priority_IS_weight': False,
+        'nstep_return': False,
+        'nstep': 3,
+        'transition_with_policy_data': True,
+        'cfg_type': 'PPOOffPolicyDict'
+    },
+    'exp_name': 'cartpole_ppo_bc_collect',
+    'seed': 0
+}
---
+++ Cartpole/PPO-GAIL/cartpole_ppo_gail_config.py
@@ -0,0 +1,80 @@
+from easydict import EasyDict
+
+
+cartpole_ppo_gail_config = dict(
+    exp_name='cartpole_ppo_gail_seed0',
+    env=dict(
+        collector_env_num=8,
+        evaluator_env_num=5,
+        n_evaluator_episode=5,
+        stop_value=195,
+        replay_path='cartpole_ppo_gail_seed0/video',
+    ),
+    reward_model=dict(
+        type='gail',
+        input_size=5,
+        hidden_size=100,
+        batch_size=100,
+        learning_rate=1e-3,
+        update_per_collect=100,
+        # Users should add their own model path here. Model path should lead to a model.
+        # Absolute path is recommended.
+        # In DI-engine, it is ``exp_name/ckpt/ckpt_best.pth.tar``.
+        # If collect_data is True, we will use this expert_model_path to collect expert data first, rather than we
+        # will load data directly from user-defined data_path
+        expert_model_path='cartpole_ppo_offpolicy_seed0/ckpt/ckpt_best.pth.tar',
+        collect_count=10000,
+    ),
+    policy=dict(
+        cuda=False,
+        model=dict(
+            obs_shape=4,
+            action_shape=2,
+            encoder_hidden_size_list=[64, 64, 128],
+            critic_head_hidden_size=128,
+            actor_head_hidden_size=128,
+            action_space='discrete',
+        ),
+        learn=dict(
+            update_per_collect=6,
+            batch_size=64,
+            learning_rate=0.001,
+            value_weight=0.5,
+            entropy_weight=0.01,
+            clip_ratio=0.2,
+            learner=dict(hook=dict(save_ckpt_after_iter=1000)),
+        ),
+        collect=dict(
+            n_sample=128,
+            unroll_len=1,
+            discount_factor=0.9,
+            gae_lambda=0.95,
+        ),
+        eval=dict(evaluator=dict(eval_freq=40, )),
+        other=dict(replay_buffer=dict(replay_buffer_size=5000))
+    ),
+)
+cartpole_ppo_gail_config = EasyDict(cartpole_ppo_gail_config)
+main_config = cartpole_ppo_gail_config
+cartpole_ppo_gail_create_config = dict(
+    env=dict(
+        type='cartpole',
+        import_names=['dizoo.classic_control.cartpole.envs.cartpole_env'],
+    ),
+    env_manager=dict(type='base'),
+    policy=dict(type='ppo_offpolicy'),
+)
+cartpole_ppo_gail_create_config = EasyDict(cartpole_ppo_gail_create_config)
+create_config = cartpole_ppo_gail_create_config
+
+if __name__ == "__main__":
+    from ding.entry import serial_pipeline_gail
+    from dizoo.classic_control.cartpole.config import cartpole_ppo_offpolicy_config, cartpole_ppo_offpolicy_create_config
+    expert_main_config = cartpole_ppo_offpolicy_config
+    expert_create_config = cartpole_ppo_offpolicy_create_config
+    serial_pipeline_gail(
+        (main_config, create_config), (expert_main_config, expert_create_config),
+        max_env_step=1000000,
+        seed=0,
+        collect_data=True
+    )
